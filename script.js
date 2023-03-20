6faf0a480e4b4d7cf834005e8123ad1eb01ffd58
Hasan Özer <64718209+ozerhasan1@users.noreply.github.com>
2023-03-20 09:00:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/*alert("Hello! I am an alert box!!");
+
+document.getElementById("demo").onmouseover = function() {mouseOver()};
+document.getElementById("demo").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+}
+
+document.getElementById("demo").onmouseover = function() {mouseOver()};
+        document.getElementById("demo").onmouseout = function() {mouseOut()};
+        
+        function mouseOver() {
+          document.getElementById("demo").style.color = "red";
+        }
+        
+        function mouseOut() {
+          document.getElementById("demo").style.color = "black";
+        }
+
+document.getElementById("menu-list").onmouseover = function() {mouseOver()};
+document.getElementById("menu-list").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById("menu-list").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("menu-list").style.color = "black";
+}*/
